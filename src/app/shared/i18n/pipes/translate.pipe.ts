e338364dacb56d2d57d244305c65d652b09ad999
Jérémie Mercier <info@dercetech.com>
2018-03-30 17:09:19 +0200

    Cleanup commented-out naked Observable-i18n service & pipe

--- src/app/shared/i18n/pipes/translate.pipe.ts
+++ src/app/shared/i18n/pipes/translate.pipe.ts
@@ -1,46 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-// import { filter, map, switchMap, tap } from 'rxjs/operators';
-// import { combineLatest } from 'rxjs/observable/combineLatest';
-import { Select } from 'ngrx-actions';
 
 import { I18nService } from '../services/i18n-service';
-// import { of } from 'rxjs/observable/of';
 
 @Pipe({
   name: 'translate$'
 })
 export class TranslatePipe implements PipeTransform {
-  @Select('interface.activeLanguage') activeLanguage$: Observable<string>;
-
-  constructor(private _i18n: I18nService) {
-    console.log('constructing this crap');
-  }
+  constructor(private _i18n: I18nService) {}
 
   transform(value, args) {
-    console.log('applying transform');
     return this._i18n.getString$(value);
-    // return combineLatest([
-    //   this._i18n.languageLoaded$.pipe(filter(loaded => loaded)),
-    //   this.activeLanguage$
-    // ]).pipe(
-    //   tap(([a, b]) => {
-    //     const abc = this._i18n.getTranslationInCurrentLanguage(value);
-    //   }),
-    //   switchMap(
-    //     ([loaded, lang]) =>
-    //       of(this._i18n.getTranslationInCurrentLanguage(value)),
-    //     (outerStreamInput, innerStreamInput) => innerStreamInput
-    //   )
-    // );
-
-    // return this._i18n.languageLoaded$.pipe(
-    //   filter(loaded => loaded),
-    //   combineLatest([this.activeLanguage$]),
-    //   switchMap(([loaded, activeLanguage]) => {
-    //     return of('abc');
-    //   })
-    //   // map( ([loaded, activeLanguage]) => this._i18n.getTranslationInCurrentLanguage(value) )
-    // );
   }
 }
--- src/app/shared/i18n/services/i18n-service.ts
+++ src/app/shared/i18n/services/i18n-service.ts
@@ -5,16 +5,7 @@ import { Select } from 'ngrx-actions';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import {
-  map,
-  concatMap,
-  delay,
-  filter,
-  switchMap,
-  tap,
-  catchError,
-  take
-} from 'rxjs/operators';
+import { map, filter, switchMap, tap, catchError, take } from 'rxjs/operators';
 
 @Injectable()
 export class I18nService {
@@ -24,28 +15,9 @@ export class I18nService {
   @Select('interface.languageLoading')
   private _languageLoading$: Observable<boolean>;
 
-  //private _currentLang: string = null;
-  //private dict: {};
   private _keys: {};
 
-  //  private _languageLoaded$ = new BehaviorSubject<boolean>(false);
-  //  public readonly languageLoaded$ = this._languageLoaded$.asObservable();
-
-  activeLanguage$ = of('en', 'fr').pipe(
-    concatMap(lang => of(lang).pipe(delay(1000)))
-  );
-
-  constructor(private http: Http) {
-    //this.currentLanguage$.pipe(filter(lang => lang && lang.length === 2)).subscribe(langCode => this.setLanguage(langCode));
-  }
-
-  // private setLanguage(langCode: string) {
-  //   if (this._currentLang === langCode) {
-  //     return;
-  //   } else {
-  //     this.loadLanguage(langCode);
-  //   }
-  // }
+  constructor(private http: Http) {}
 
   public switchToLanguage$(langCode: string): Observable<boolean> {
     return this.http
@@ -63,24 +35,10 @@ export class I18nService {
       filter(loading => loading === false),
       switchMap(
         () => this._currentLanguage$.pipe(take(1)),
-        (loading, currentLanguage) => {
-          console.log('crap');
-          return this.getTranslation(ref, currentLanguage);
-        }
+        (loading, currentLanguage) => this.getTranslation(ref, currentLanguage)
       )
     );
   }
-
-  // private loadLanguage(langCode: string) {
-  //   this.http
-  //     .get(`./assets/i18n/${langCode}.json`)
-  //     .pipe(map((res: any) => res.json()))
-  //     .subscribe(
-  //       translations => (this.dict = translations),
-  //       () => {},
-  //       () => this._languageLoaded$.next(true)
-  //     );
-  // }
 
   private getTranslation(ref: any, langId: string): string {
     if (this.isObject(ref)) {
@@ -117,57 +75,6 @@ export class I18nService {
 
     return <string>level;
   }
-
-  // getActiveLanguage(): string {
-  //   return this._currentLang;
-  // }
-
-  // getAvailableLanguages(): string[] {
-  //   return this._availableLangs;
-  // }
-
-  /*
-  getTranslationInCurrentLanguage(ref: any): string {
-    // Is string or description translation object?
-    if (this.isObject(ref)) {
-      if (ref.hasOwnProperty(this._currentLang)) {
-        if (ref[this._currentLang]) return ref[this._currentLang];
-        else if (ref['en']) ref['en'];
-        else if (ref['fr']) ref['fr'];
-        else {
-          const langs = Object.keys(ref);
-          if (langs && langs.length > 0) return ref[langs[0]];
-          else return 'no translation';
-        }
-      } else return '';
-    } else {
-      return this.getTranslationFromDictionaryInCurrentLanguage(ref);
-    }
-  }
-
-  private getTranslationFromDictionaryInCurrentLanguage(ref: string): string {
-    if (!ref) return '';
-    if (!this.dict) return ref;
-
-    const segments: string[] = ref.split('.');
-
-    let key,
-      level = this.dict;
-    while ((key = segments.shift())) {
-      level = level[key];
-      if (!level) {
-        console.warn(
-          'i18n missing key "' + ref + '" in lang ' + this._currentLang
-        );
-        level = ref;
-        break;
-      }
-    }
-
-    return <string>level;
-  }
-
-  */
 
   private isObject(obj): boolean {
     return obj === Object(obj);
