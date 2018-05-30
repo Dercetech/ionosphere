a411d783840e67e398fb9356bec22599d52645e8
Jérémie Mercier <info@dercetech.com>
2018-05-30 23:48:23 +0200

    Synchronized store tweak

--- src/app/shared/store/classes/sychronized-store.actions.ts
+++ src/app/shared/store/classes/sychronized-store.actions.ts
@@ -1,14 +1,14 @@
 import { TypedAction } from './typed-action';
 
 export class CollectionMonitoringRequestAction extends TypedAction {
-  static TYPE = '[Monitoring] Monitoring requested';
+  static TYPE = '[Database] Monitoring requested';
   constructor(public payload: { collectionKey: string }) {
     super();
   }
 }
 
 export class CollectionMonitoringReleaseAction extends TypedAction {
-  static TYPE = '[Monitoring] Monitoring released';
+  static TYPE = '[Database] Monitoring released';
   constructor(public payload: { collectionKey: string }) {
     super();
   }
